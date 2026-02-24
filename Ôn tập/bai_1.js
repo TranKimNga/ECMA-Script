8c4acfc1aba7978dc41beb4e079305bfc8428b51
Tran Kim Nga <trannga.tkn@gmail.com>
2026-02-24 20:45:32 +0700

    Session 5 - Review

---
+++ Ôn tập/bai_1.js
@@ -0,0 +1,9 @@
+let time = 10;
+while (time > 0) {
+    if (time === 5) {
+        console.log("Da qua nua thoi gian");
+    } else {
+        console.log(`Con lai ${time} giay`);
+    }
+}
+console.log("Het gio");
