87debf32e3b58fe1eb7a36ccbc03fbed9abb1f7f
Tran Kim Nga <trannga.tkn@gmail.com>
2026-02-26 20:50:50 +0700

    day 6

---
+++ DAY 6/return.js
@@ -0,0 +1,7 @@
+function calculatePytago(canhGocVuong1, canhGocVuong2) {
+    console.log(
+        Math.sqrt(Math.pow(canhGocVuong1, 2) + Math.pow(canhGocVuong2, 2)),
+    );
+}
+
+calculatePytago(10, 20);
