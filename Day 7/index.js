ec94bbbe6ac9a8d8ac5bc9ff6c2b2d92fe980331
Tran Kim Nga <trannga.tkn@gmail.com>
2026-02-28 20:18:38 +0700

    day 7

--- Day 7/index.js
+++ Day 7/index.js
@@ -8,8 +8,10 @@ let carts = [
     "Kem",
     "Dầu gội",
     "Sữa rửa mặt",
-    "Kẹo",
+    "Kem",
 ];
 
 console.log(`Tổng số món hàng: ${carts.length}`);
-console.log(`Món hàng đầu tiên: ${carts[0]} và sau cùng: ${carts[carts.length -1]}`)
+console.log(
+    `Món hàng đầu tiên: ${carts[0]} và sau cùng: ${carts[carts.length - 1]}`,
+);
