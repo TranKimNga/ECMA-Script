840a312687d2877311707ecd7e5a1405085a57e6
Tran Kim Nga <trannga.tkn@gmail.com>
2026-03-03 20:26:06 +0700

    day 8

---
+++ Day 8/filter.js
@@ -0,0 +1,11 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.forEach((number, index) => {
+    console.log(number);
+});
+
+const filteredNumbers = numbers.filter((number, index) => {
+    return number % 2 === 0;
+});
+
+console.log(filteredNumbers);
---
+++ Day 8/forEach.js
@@ -0,0 +1,35 @@
+/* 
+1. forEach  => Lặp qua từng phần tử
+2. filter   => Lọc phần tử
+*/
+
+/*
+1. forEach
+numbers.forEach(loopNumbersArray);
+
+const provinces = [
+    "Hà Nội",
+    "Đà Lạt",
+    "TP. Hồ Chí Minh",
+    "Đà Nẵng",
+    "Cần Thơ",
+    "Huế",
+];
+
+provinces.forEach((province) => {
+    console.log(province);
+});
+*/
+
+// 2. filter
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+numbers.forEach((number, index) => {
+    console.log(number);
+});
+
+const filteredNumbers = numbers.filter((number, index) => {
+    return number % 2 === 0;
+});
+
+console.log(filteredNumbers);
